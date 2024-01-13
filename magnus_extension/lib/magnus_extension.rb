2a0f3d1bd5b136850189e3ae29f793e59968d60e
Filip Pacanowski <fpacanowski@gmail.com>
2024-01-13 15:36:16 +0100

    Magnus-based gem boilerplate.

---
+++ magnus_extension/lib/magnus_extension.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "magnus_extension/version"
+require_relative "magnus_extension/magnus_extension"
+
+module MagnusExtension
+  class Error < StandardError; end
+  # Your code goes here...
+end
