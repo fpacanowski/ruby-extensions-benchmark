2bee72f4bf794ea295bfadddf1e41c4da8e0e2d7
Filip Pacanowski <fpacanowski@gmail.com>
2024-01-13 14:14:58 +0100

    C implementation

--- c_extension/ext/c_extension/c_extension.c
+++ c_extension/ext/c_extension/c_extension.c
@@ -1,9 +1,28 @@
 #include "c_extension.h"
 
-VALUE rb_mCExtension;
+char PAYLOAD[] = "ABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABC";
+
+VALUE rb_mCext;
+
+VALUE build_tree(int depth) {
+  VALUE result = rb_hash_new();
+  VALUE children = rb_ary_new();
+  if(depth != 1) {
+    rb_ary_push(children, build_tree(depth-1));
+    rb_ary_push(children, build_tree(depth-1));
+  }
+  rb_hash_aset(result, ID2SYM(rb_intern("label")), rb_str_new_cstr(PAYLOAD));
+  rb_hash_aset(result, ID2SYM(rb_intern("children")), children);
+  return result;
+}
+
+VALUE build_big_tree() {
+  return build_tree(13);
+}
 
 void
 Init_c_extension(void)
 {
-  rb_mCExtension = rb_define_module("CExtension");
+  rb_mCext = rb_define_module("CExtension");
+  rb_define_module_function(rb_mCext, "build_big_tree", build_big_tree, 0);
 }
