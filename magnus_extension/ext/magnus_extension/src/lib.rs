ebb731afaa2c0ccc0d3378d59e83290f546faecd
Filip Pacanowski <fpacanowski@gmail.com>
2024-01-13 15:41:56 +0100

    Rust implementation

--- magnus_extension/ext/magnus_extension/src/lib.rs
+++ magnus_extension/ext/magnus_extension/src/lib.rs
@@ -1,12 +1,26 @@
-use magnus::{define_module, function, prelude::*, Error};
+use magnus::{define_module, function, prelude::*, Error, RHash, RArray, Symbol};
 
-fn hello(subject: String) -> String {
-    format!("Hello from Rust, {}!", subject)
+static PAYLOAD: &str = "ABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABC";
+
+fn build_tree(depth: i32) -> RHash {
+    let result = RHash::new();
+    result.aset(Symbol::new("label"), PAYLOAD).unwrap();
+    let children = RArray::new();
+    if depth != 1 {
+        children.push(build_tree(depth - 1)).unwrap();
+        children.push(build_tree(depth - 1)).unwrap();
+    }
+    result.aset(Symbol::new("children"), children).unwrap();
+    return result;
+}
+
+fn build_big_tree() -> RHash {
+    return build_tree(13);
 }
 
 #[magnus::init]
 fn init() -> Result<(), Error> {
     let module = define_module("MagnusExtension")?;
-    module.define_singleton_method("hello", function!(hello, 1))?;
+    module.define_singleton_method("build_big_tree", function!(build_big_tree, 0))?;
     Ok(())
 }
