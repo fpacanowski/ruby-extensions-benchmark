20ba60ff438f7e67b028dfeed26d1c412a5e2aa6
Mat Sadler <mat@sourcetagsandcodes.com>
2024-01-18 08:37:04 -0800

    update to newer magnus apis

--- magnus_extension/ext/magnus_extension/src/lib.rs
+++ magnus_extension/ext/magnus_extension/src/lib.rs
@@ -1,26 +1,26 @@
-use magnus::{define_module, function, prelude::*, Error, RHash, RArray, Symbol};
+use magnus::{function, prelude::*, Error, RHash, Ruby};
 
 static PAYLOAD: &str = "ABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABC";
 
-fn build_tree(depth: i32) -> RHash {
-    let result = RHash::new();
-    result.aset(Symbol::new("label"), PAYLOAD).unwrap();
-    let children = RArray::new();
+fn build_tree(ruby: &Ruby, depth: i32) -> RHash {
+    let result = ruby.hash_new();
+    result.aset(ruby.to_symbol("label"), PAYLOAD).unwrap();
+    let children = ruby.ary_new();
     if depth != 1 {
-        children.push(build_tree(depth - 1)).unwrap();
-        children.push(build_tree(depth - 1)).unwrap();
+        children.push(build_tree(ruby, depth - 1)).unwrap();
+        children.push(build_tree(ruby, depth - 1)).unwrap();
     }
-    result.aset(Symbol::new("children"), children).unwrap();
+    result.aset(ruby.to_symbol("children"), children).unwrap();
     return result;
 }
 
-fn build_big_tree() -> RHash {
-    return build_tree(13);
+fn build_big_tree(ruby: &Ruby) -> RHash {
+    return build_tree(ruby, 13);
 }
 
 #[magnus::init]
-fn init() -> Result<(), Error> {
-    let module = define_module("MagnusExtension")?;
+fn init(ruby: &Ruby) -> Result<(), Error> {
+    let module = ruby.define_module("MagnusExtension")?;
     module.define_singleton_method("build_big_tree", function!(build_big_tree, 0))?;
     Ok(())
 }
